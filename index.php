7ff25efb4ae943fc264f946455bc234e1b831429
JohnathanDevelops <JohnathanDevelops@users.noreply.github.com>
2014-10-29 19:59:27 -0400

    index.php
    Main Page ;)

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>John-Designs</title>
+<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<body>
+<form id="menubar" method="post" action="">
+<a href="#" class="homebutton">Home</a>
+</form>
+<form id="body" method="post" action="">
+</form>
+</body>
+</html>
